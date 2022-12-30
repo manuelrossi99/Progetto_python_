8252028bb030821e550caf4c5319e45a20ce7007
Manuel Rossi <79398950+manuelrossi99@users.noreply.github.com>
2022-12-30 11:24:23 +0100

    streamlit test

--- project.py
+++ project.py
@@ -1,2 +1,10 @@
-def hello():
-    print("hello World")
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib as mt
+
+st.header("hoi")
+st.write("hoi wia geats")
+st.write("hoi wia geats")
+
+
