118094e87f9aa10956002cfac8ce1e2df0848e8b
Manuel Rossi <79398950+manuelrossi99@users.noreply.github.com>
2023-01-28 18:08:00 +0100

    fixing object columns to floats

--- main.py
+++ main.py
@@ -4,100 +4,122 @@ import numpy as np
 import matplotlib.pyplot as plt
 import glob
 import seaborn as sn
+import statsmodels.api as sm
 
-#Start to create the Dataframe by using Data that can be imported and merged automaticly
+
+# Start to create the Dataframe by using Data that can be imported and merged automaticly
 path = "Data"
 file_path = glob.glob(path + "/*.csv")
 
-#Using encoding for German letters 
-list_of_df_files = map(lambda file_path: pd.read_csv(file_path, sep = ";", encoding = 'utf-8', decimal=",", thousands= "."), file_path)
+# Using encoding for German letters
+list_of_df_files = map(lambda file_path: pd.read_csv(
+    file_path, sep=";", encoding='utf-8', decimal=",", thousands="."), file_path)
 list_of_df_files = list(list_of_df_files)
-#print(list_of_df_files)
+# print(list_of_df_files)
 
-#Checking if imports went well
+# Checking if imports went well
 '''
 for i in list_of_df_files:
   i.info()
 '''
 
-#without endcoding some more cleaning is necessary
+# without endcoding some more cleaning is necessary
 '''
 for i in list_of_df_files:
   i.columns= i.columns.str.replace('[ï»¿]', '')
 '''
-#creating a function to merge my dataframes
+# creating a function to merge my dataframes
+
+
 def merge_df(dataframes, column_names):
     result = dataframes[0]
     for i in range(1, len(dataframes)):
         result = pd.merge(result, dataframes[i], on=column_names)
     return result
 
-#merging acctual dataframes on municipality_id and municipality name 
+
+# merging acctual dataframes on municipality_id and municipality name
 columns = ("municipality_id",	"municipality_name")
 data_df = merge_df(list_of_df_files, columns)
 
 
-#importing csv, which needed some more cleaning before merging
-covid_doses_municipalities = pd.read_csv("COVID19_vaccination_municipalities_doses_timeline.csv", sep = ";")
+# importing csv, which needed some more cleaning before merging
+covid_doses_municipalities = pd.read_csv(
+    "COVID19_vaccination_municipalities_doses_timeline.csv", sep=";", thousands=".", decimal="," )
 covid_doses_municipalities_df = pd.DataFrame(covid_doses_municipalities)
-mask_last_dates = covid_doses_municipalities_df["date"] == "2022-02-01" #using only newest data
+# using only newest data
+mask_last_dates = covid_doses_municipalities_df["date"] == "2022-02-01"
 covid_doses_municipalities_df = covid_doses_municipalities_df[mask_last_dates]
-covid_doses_municipalities_df= covid_doses_municipalities_df.drop("date", axis = 1 )
+covid_doses_municipalities_df = covid_doses_municipalities_df.drop(
+    "date", axis=1)
 covid_doses_municipalities_df = covid_doses_municipalities_df.reset_index()
-covid_doses_municipalities_df = covid_doses_municipalities_df.drop("index", axis =1)
-#covid_doses_municipalities_df.info()
+covid_doses_municipalities_df = covid_doses_municipalities_df.drop(
+    "index", axis=1)
+# covid_doses_municipalities_df.info()
 
-#merge the new df to the other 
+# merge the new df to the other
 new_list_to_merge = (data_df, covid_doses_municipalities_df)
 data_df = merge_df(new_list_to_merge, "municipality_id")
-#data_df.info()
+# data_df.info()
 
-antivax_docs = pd.read_csv("antivax_docs.csv", sep = ";")
+antivax_docs = pd.read_csv("antivax_docs.csv", sep=";", thousands=".", decimal=",")
 antivax_docs_df = pd.DataFrame(antivax_docs)
-antivax_docs_df= antivax_docs_df.groupby("municipality_id", group_keys=False).sum().reset_index()
+antivax_docs_df = antivax_docs_df.groupby(
+    "municipality_id", group_keys=False).sum().reset_index()
 antivax_docs_df.columns
-#antivax_docs_df.info())
+# antivax_docs_df.info())
 
-#merge and fill municipalities with no antivax docs with 0 
-data_df = data_df.merge(antivax_docs_df,how='left', left_on='municipality_id', right_on='municipality_id')
+# merge and fill municipalities with no antivax docs with 0
+data_df = data_df.merge(antivax_docs_df, how='left',
+                        left_on='municipality_id', right_on='municipality_id')
 data_df = data_df.fillna(0)
 
 
-urban_rural = pd.read_csv("urban_rural.csv", sep = ";")
+urban_rural = pd.read_csv("urban_rural.csv", sep=";", thousands=".", decimal=",")
 urban_rural_df = pd.DataFrame(urban_rural)
 urban_rural_df = urban_rural_df.drop("municipality_name", axis=1)
-data_df = data_df.merge(urban_rural_df,how='left', left_on='municipality_id', right_on='municipality_id')
-data_df = data_df.fillna(1) #since missing values are from Vienna wich is an urban city categoriesed as 1
+data_df = data_df.merge(urban_rural_df, how='left',
+                        left_on='municipality_id', right_on='municipality_id')
+# since missing values are from Vienna wich is an urban city categoriesed as 1
+data_df = data_df.fillna(1)
 
-political_parties = pd.read_csv("nrw_2019_r.csv", sep =";", encoding = 'utf-8')
+political_parties = pd.read_csv("nrw_2019_r.csv", sep=";", encoding='utf-8', thousands=",", decimal=".")
 political_parties_df = pd.DataFrame(political_parties)
-political_parties_df = political_parties_df.drop(["Gebietsname", "eligible_voters", "retrieved", "invalid", "valid"], axis= 1)
+political_parties_df = political_parties_df.drop(
+    ["Gebietsname", "eligible_voters", "retrieved", "invalid", "valid"], axis=1)
 new_list_to_merge = (data_df, political_parties_df)
 data_df = merge_df(new_list_to_merge, "municipality_id")
-#checking if all values are not fine:
+
+
+
+# checking if all values are not fine:
 
 data_df.isnull().sum()
 data_df.isna().sum()
-#print(data_df.dtypes) #checking if all columns are numeric 
+# print(data_df.dtypes) #checking if all columns are numeric
+#pop_uni didnt read right so i have to change the object to float
+data_df["sh_pop_uni"] = data_df["sh_pop_uni"].apply(pd.to_numeric, errors='coerce')
+data_df["pop_uni"] = data_df["pop_uni"].apply(pd.to_numeric, errors='coerce')
 
-#streamlit!!!!!!!!
-#correlation matrix 
-#corr_matrix = data_df.corr()
-#sn.heatmap(corr_matrix, annot=True)
-#plt.show()
 
-data_df["dose_1_sh"]= data_df["dose_1"]/data_df["municipality_population"]
-data_df["dose_2_sh"]= data_df["dose_2"]/data_df["municipality_population"]
-data_df["dose_3_sh"]= data_df["dose_3"]/data_df["municipality_population"]
+# streamlit!!!!!!!!
+# correlation matrix
+# corr_matrix = data_df.corr()
+# sn.heatmap(corr_matrix, annot=True)
+# plt.show()
+
+data_df["dose_1_sh"] = data_df["dose_1"]/data_df["municipality_population"]
+data_df["dose_2_sh"] = data_df["dose_2"]/data_df["municipality_population"]
+data_df["dose_3_sh"] = data_df["dose_3"]/data_df["municipality_population"]
 
 
 mask_vaccine_skeptice = data_df["vaccine_skeptic"] > 0
-mask_not_vaccine_skeptice = data_df["vaccine_skeptic"] == 0 
+mask_not_vaccine_skeptice = data_df["vaccine_skeptic"] == 0
 data_df_vaccine_skeptic = data_df[mask_vaccine_skeptice]
 data_df_not_vaccine_skeptic = data_df[mask_not_vaccine_skeptice]
 
 
-#plots
+# plots
 mean_vaccine_dose1_vs = data_df_vaccine_skeptic["dose_1_sh"].mean()
 mean_vaccine_dose2_vs = data_df_vaccine_skeptic["dose_2_sh"].mean()
 mean_vaccine_dose3_vs = data_df_vaccine_skeptic["dose_3_sh"].mean()
@@ -106,8 +128,11 @@ mean_vaccine_dose1_nvs = data_df_not_vaccine_skeptic["dose_1_sh"].mean()
 mean_vaccine_dose2_nvs = data_df_not_vaccine_skeptic["dose_2_sh"].mean()
 mean_vaccine_dose3_nvs = data_df_not_vaccine_skeptic["dose_3_sh"].mean()
 
-means_vs = [mean_vaccine_dose1_vs,mean_vaccine_dose2_vs,mean_vaccine_dose3_vs]
-means_nvs = [mean_vaccine_dose1_nvs,mean_vaccine_dose2_nvs,mean_vaccine_dose3_nvs]
+means_vs = [mean_vaccine_dose1_vs,
+            mean_vaccine_dose2_vs, mean_vaccine_dose3_vs]
+means_nvs = [mean_vaccine_dose1_nvs,
+             mean_vaccine_dose2_nvs, mean_vaccine_dose3_nvs]
+
 '''
 fig, ax = plt.subplots()
 index = np.arange(3)
@@ -131,8 +156,9 @@ plt.xticks(index + bar_width, ('Dose_1', 'Dose_2', 'Dose_3'))
 plt.legend()
 plt.show()
 '''
-##plot2
 
+# plot2
+'''
 mask_vaccine_skeptice_and_rural = (data_df["vaccine_skeptic"] > 1) & (data_df["type_urban_rural"] > 1)
 data_df_vaccine_skeptic_and_rural = data_df[mask_vaccine_skeptice_and_rural]
 
@@ -155,7 +181,7 @@ label='VaccineSkepticeDoc_rural')
 
 rects2 = plt.bar(index + bar_width, means_nvs, bar_width,
 alpha=opacity,
-color='g',
+color='lightblue',
 label='Other')
 
 plt.xlabel('Rural municipalities with or without Vs Doc')
@@ -164,6 +190,28 @@ plt.title('Vaccinationrates in Comparison for rural municipalities with or witho
 plt.xticks(index + bar_width, ('Dose_1', 'Dose_2', 'Dose_3'))
 plt.legend()
 plt.show()
+'''
 
 
 
+
+'''
+#Models
+y =  data_df["dose_2"].astype(int)
+x = np.array([data_df["pensions"], data_df["vaccine_skeptic"], data_df["FPOE_per"], data_df["sh_pop_0_14"],
+     data_df["sh_pop_15_29"], data_df["sh_pop_30_44"], data_df["sh_pop_45_59"], data_df["sh_pop_60_74"],
+     data_df["sh_pop_75_99"], data_df["sh_pop_foreign"], data_df["sh_pop_lehre"],
+     data_df["sh_pop_pflichtschule"], data_df["sh_pop_uni"], data_df["salaries"],
+     data_df["type_urban_rural"]])
+
+model = sm.OLS(y,x.astype(float))
+
+results = model.fit()
+#print(results.summary())
+'''
+
+#print(data_df["dose_2"])
+
+
+
+print(data_df.info())
